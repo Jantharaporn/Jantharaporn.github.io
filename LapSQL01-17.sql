0d3084b087fec51db14fabb43bb9d0dd723e4779
Jantharaporn <chantarapon48@gmail.com>
2025-07-17 17:11:53 +0700

    Add files via upload

--- LapSQL01-17.sql
+++ LapSQL01-17.sql
@@ -5,6 +5,11 @@ Select EmployeeID , FirstName , LastName from Employees
 --พนักงานที่อยู่เมือง london
 Select * from Employees where city = 'london'
 
+--แสดงชื่อเมือง และประเทศทั้งหมดของลูกค้า
 Select City, Country from Customers
-
+--ISTINCT ข้อมูลที่แสดงออกมาซ้ำกันจะแสดงเพียง 1 แถว
 Select Distinct City, Country from Customers
+--แสดงข้อมูลสินค้าที่มีราคาสูงกว่า 200
+SELECT * FROM Products WHERE Unitprice > 200
+--แสดงข้อมูลลูกค้าที่ตั้งอยู่ใน London หรือ Vancouver
+SELECT * FROM Customers WHERE City ='London' OR City ='Vancouver'
